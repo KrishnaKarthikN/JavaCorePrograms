c47c7723165d536dee47a363f228ba13c4b9272a
Krishna Karthik N <71656457+KrishnaKarthikN@users.noreply.github.com>
2020-10-21 07:56:43 +0530

    Commit 1

--- com/xworkz/Assignments/InheritanceAM/ToolKit.java
+++ com/xworkz/Assignments/InheritanceAM/ToolKit.java
@@ -8,9 +8,9 @@ public class ToolKit {
 	private void hasScrewBits() {
 		System.out.println("Kit contains all drilling bit ");//Cannot be instantiated in child class by creating an instance
 	}
-//	default void hammerDrill() {
-//		System.out.println("Kit contains 550W 10mm Hammer Drill ");//Default methods are allowed only in interfaces.
-//	}	
+	void hammerDrill() {
+		System.out.println("Kit contains 550W 10mm Hammer Drill ");//Default methods are allowed only in interfaces.
+	}	
 	protected void hasAllenKeys() {
 		System.out.println("kit Contains Allen keys");
 	}
