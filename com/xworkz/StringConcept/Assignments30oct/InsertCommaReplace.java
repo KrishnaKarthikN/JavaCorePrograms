4aed66514995a72c990ecdd098b7fc8be5c40c55
Krishna Karthik N <71656457+KrishnaKarthikN@users.noreply.github.com>
2020-10-31 09:27:32 +0530

    Commit 1

---
+++ com/xworkz/StringConcept/Assignments30oct/InsertCommaReplace.java
@@ -0,0 +1,15 @@
+package com.xworkz.StringConcept.Assignments30oct;
+
+public class InsertCommaReplace {
+	public static void main(String[] args) {
+	
+
+		String str = "Welcome to xworkz and you are learning string methods in java";
+		//String[] str = s.split(" ");
+		String s = str.replace(' ' , ',');
+		System.out.println(s);
+		
+		
+	}
+}
+//Output:Welcome,to,xworkz,and,you,are,learning,string,methods,in,java
